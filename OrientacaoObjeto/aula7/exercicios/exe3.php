4693c1ab277918f70af11fdb9dc0c3ee3fe81fcd
GustavoGussolli <134729255+GustavoGussolli@users.noreply.github.com>
2024-09-06 10:17:27 -0300

    Add files via upload

--- OrientacaoObjeto/aula7/exercicios/exe3.php
+++ OrientacaoObjeto/aula7/exercicios/exe3.php
@@ -1,101 +1,83 @@
-<?php 
+<?php
 
-    class Posto{
+class Posto {
+    private $litrosGasolina;
+    private $abastecimentosGasolina;
 
-        private $litrosGasolina;
-        private $abastecimentosGasolina;
-
-        public function __construct(){
-            
-            $this->litrosGasolina = 0;
-            $this->abastecimentosGasolina = array();
-            
-        }
-
-        public function abastecer($litros){
-
-            if($this->litrosGasolina >= $litros){
-
-                return true;
-
-            } else {
-                return false;
-            }
-            
-        }
-
-        public function reporEstoque($litros){
-
-            $this->litrosGasolina += $litros;
-
-        }
-
-        /**
-         * Get the value of abastecimentosGasolina
-         */
-        public function getAbastecimentosGasolina()
-        {
-                return $this->abastecimentosGasolina;
-        }
-
-        /**
-         * Set the value of abastecimentosGasolina
-         */
-        public function setAbastecimentosGasolina($abastecimentosGasolina): self
-        {
-                $this->abastecimentosGasolina = $abastecimentosGasolina;
-
-                return $this;
-        }
+    public function __construct() {
+        $this->litrosGasolina = 0;
+        $this->abastecimentosGasolina = array();
     }
 
-    $posto = new Posto();
+    public function abastecer($litros) {
+        
+        if($this->litrosGasolina >= $litros) {
+            $this->litrosGasolina -= $litros;
+            array_push($this->abastecimentosGasolina, $litros);
 
-    $opcao = 0;
-        do {
+            return true;
+        }
 
-            echo "\n-----------MENU-----------\n";
-            echo "1- Abastecer\n";
-            echo "2- Repor estoque\n";
-            echo "3- Listar abastecimentos\n";
-            echo "0- SAIR\n";
+        return false;
+    }
 
-        $opcao = readline("Escolha a opção: ");
+    public function reporEstoque($litros) {
+        $this->litrosGasolina += $litros;
+    }
 
-        switch($opcao) {
-            case 0:
-                echo "Programa encerrado!\n";
-                break;
+    /**
+     * Get the value of abastecimentosGasolina
+     */
+    public function getAbastecimentosGasolina()
+    {
+        return $this->abastecimentosGasolina;
+    }
+}
 
-            case 1:
-                echo "Abastecer\n";
-                $litros = readline("Informe quantos litros vai abastecer: ");
-                $deuCerto = $posto->abastecer($litros);
+$posto = new Posto();
 
-                if($deuCerto == true){
-                    echo "Abastecimento realizado com sucesso";
-                } 
+do {
+    echo "\n-----------MENU-----------\n";
+    echo "1- Abastecer\n";
+    echo "2- Repor estoque\n";
+    echo "3- Listar abastecimentos\n";
+    echo "0- Sair\n";
 
-                if($deuCerto == false){
-                    echo "Abastecimento não foi realizado com sucesso";
-                }
-                break;
+    $opcao = readline("Informe a opção: ");
+    echo "\n"; //Apenas para executar uma quebra de linha
 
-             case 2:
-                echo "Repor Estoque\n";
-                $litros = readline("Informe quantos litros vai repor: ");
-                $posto->reporEstoque($litros);
-                break;
+    switch($opcao) {
+        case 1:
+            $litros = readline("Informe a quantidade de litros: ");
+            $deuCerto = $posto->abastecer($litros);
+            if($deuCerto) {
+                echo "Abastecimento realizado com sucesso!\n";
+            } else
+                echo "Não foi possível abastecer por falta de gasolina!\n";
 
-            case 3;
-                echo "Listar abastecimentos \n";
-                foreach ($this->abastecimentosGasolina as $a){
-                    echo $a;
-                }
-                break;
+            break;
 
-            default:
-                echo "Opção INVÁLIDA!\n";
+        case 2: 
+            //Repor estoque
+            $litros = readline("Informe a quantidade de litros: ");
+            $posto->reporEstoque($litros);
 
-     }
-    } while($opcao != 0);
+            break;
+
+        case 3:
+            $abastecimentos = $posto->getAbastecimentosGasolina();
+            for($i=0; $i<count($abastecimentos); $i++)
+                printf("- Abastecimento %d: %d litros\n", $i+1, $abastecimentos[$i]);
+
+            break;
+
+        case 0:
+            echo "Programa encerrado!\n";
+            break;
+
+        default:
+            echo "Opção Inválida!\n";
+            break;
+    }
+
+} while($opcao != 0);
